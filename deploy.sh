9b7c7d02af3843eaeefa8c4b6c4d203cc16476b4
gekkin-programmer <nkouambrayan@gmail.com>
2026-03-06 04:16:26 +0100

    fix: add prisma generate to deploy script before api build

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ cd /root/app
 git pull origin master
 pnpm install --frozen-lockfile
 pnpm --filter shared build
+pnpm --filter api run db:generate
 pnpm --filter api build
 pnpm --filter web build
 pnpm --filter api exec prisma migrate deploy
